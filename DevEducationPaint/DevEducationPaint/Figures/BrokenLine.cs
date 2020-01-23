1d34d704e2e4d8336db7ca070f3bfbaef69d0732
sereja <serejavarenick@yandex.ru>
2020-01-23 18:00:17 +0300

    возможно, этот коммит и не нужен. но тут что-то про BrokenLine

---
+++ DevEducationPaint/DevEducationPaint/Figures/BrokenLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Text;
+using System.Windows.Media.Imaging;
+using DevEducationPaint.Drawers;
+using DevEducationPaint.Strategies;
+using Point = System.Windows.Point;
+
+namespace DevEducationPaint.Figures
+{
+  class BrokenLine : MainWindow
+  {
+    private RastrDrawer drawer = RastrDrawer.GetDrawer();
+    public WriteableBitmap DrawAlgorithm(WriteableBitmap bitmap, System.Windows.Point startPoint, System.Windows.Point endPoint)
+    {
+      Point one = new Point(startPoint.X, startPoint.Y);
+      bitmap = drawer.DrawLine(startPoint, endPoint, bitmap);
+      return bitmap;
+      
+    }
+  }
+}
