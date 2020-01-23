84d93b2ea9a2d3a3b201bb1299553ce7236d3ee9
Levi <Laytika@icloud.com>
2020-01-23 13:47:58 +0300

    всё ещё новый интерфейс

---
+++ DevEducationPaint/DevEducationPaint/Figures/FigureEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationPaint.Figures
+{
+    public enum FigureEnum
+    {
+        Line = 1,
+        Square,
+        Circle,
+        Triangle,
+        Polygone
+    }
+}
