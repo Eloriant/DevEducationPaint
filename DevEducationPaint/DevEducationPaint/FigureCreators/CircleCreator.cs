62b40d3b1f4f2945071bc59caa549c0c3cf3c7b6
evsigneev <evsigneev.a@mail.ru>
2020-01-29 18:38:32 +0300

    Работает круг + дефолтный цвет рисования

--- DevEducationPaint/DevEducationPaint/FigureCreators/CircleCreator.cs
+++ DevEducationPaint/DevEducationPaint/FigureCreators/CircleCreator.cs
@@ -11,9 +11,9 @@ namespace DevEducationPaint.FigureCreators
     {
         public override Figure CreateFigure(Point start, Point end)
         {
-            int cx = start.X;
-            int cy = start.Y;
-            int r = Math.Abs(end.X - start.X);
+            int cx =  start.X;
+            int cy =  start.Y;
+            int r =  Math.Abs(end.X - start.X);
 
             List<Point> arcPoints = new List<Point>();
             for (int i = 0; i <= r; i++)
@@ -22,20 +22,21 @@ namespace DevEducationPaint.FigureCreators
             }
 
             List<Point> circlePoints = new List<Point>();
-            for (int i = 0; i < arcPoints.Count; i++)
+            List<Point> circlePoints1 = new List<Point>();
+            List<Point> circlePoints2 = new List<Point>();
+            List<Point> circlePoints3 = new List<Point>();
+            List<Point> circlePoints4 = new List<Point>();
+            for (int i = 1; i < arcPoints.Count; i++)
             {
-                circlePoints.Add(new Point(arcPoints[i].X + cx, arcPoints[i].Y + cy));
-                new Point(arcPoints[i].X + cx, arcPoints[i].Y + cy);
-                circlePoints.Add(new Point(arcPoints[i - 1].X + cx, Math.Abs(arcPoints[i - 1].Y - cy)));
-                new Point(arcPoints[i].X + cx, Math.Abs(arcPoints[i].Y - cy));
-                circlePoints.Add(new Point(Math.Abs(arcPoints[i - 1].X - cx), arcPoints[i - 1].Y + cy));
-                new Point(Math.Abs(arcPoints[i].X - cx), arcPoints[i].Y + cy);
-                circlePoints.Add(new Point(Math.Abs(arcPoints[i - 1].X - cx), Math.Abs(arcPoints[i - 1].Y - cy)));
-                new Point(Math.Abs(arcPoints[i].X - cx), Math.Abs(arcPoints[i].Y - cy));
-
-                // рассчитать точки для остальных дуг
+                circlePoints1.Add(new Point(arcPoints[i].X + cx, arcPoints[i].Y + cy));
+                circlePoints2.Add(new Point(arcPoints[arcPoints.Count - i - 1].X + cx, Math.Abs(arcPoints[arcPoints.Count - i - 1].Y - cy)));
+                circlePoints3.Add(new Point(Math.Abs(arcPoints[i - 1].X - cx), Math.Abs(arcPoints[i - 1].Y - cy)));
+                circlePoints4.Add(new Point(Math.Abs(arcPoints[arcPoints.Count - i - 1].X - cx), Math.Abs(arcPoints[arcPoints.Count - i - 1].Y + cy)));
             }
-               
+            circlePoints.AddRange(circlePoints1);
+            circlePoints.AddRange(circlePoints2);
+            circlePoints.AddRange(circlePoints3);
+            circlePoints.AddRange(circlePoints4);
 
             return new CircleFigure(circlePoints);
         }
