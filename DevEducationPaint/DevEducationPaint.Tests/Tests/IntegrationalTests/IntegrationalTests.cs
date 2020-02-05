269e98ee2bf6867b9b28df88437c2c5b79c859d0
Valya <valentinaburdina1994@gmail.com>
2020-02-05 21:31:10 +0300

    добавила еще пару тестов

--- DevEducationPaint/DevEducationPaint.Tests/Tests/IntegrationalTests/IntegrationalTests.cs
+++ DevEducationPaint/DevEducationPaint.Tests/Tests/IntegrationalTests/IntegrationalTests.cs
@@ -10,46 +10,88 @@ namespace DevEducationPaint.Tests.Tests.IntegrationalTests
     class IntegrationalTests
     {
         [TestCase()]
-        public void BrokenLineCreatorTest()
+        public void BrokenLineCreatorTest_WithTrueAssert()
         {
             BrokenLineFigure brokenLine = new BrokenLineFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(brokenLine is BrokenLineFigure);
         }
         [TestCase()]
-        public void CircleFigureCreatorTest()
+        public void BrokenLineCreatorTest_WithFalseAssert()
+        {
+            BrokenLineFigure brokenLine = new BrokenLineFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(brokenLine is CircleFigure);
+        }
+        [TestCase()]
+        public void CircleFigureCreatorTest_WithTrueAssert()
         {
             CircleFigure circleFigure = new CircleFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(circleFigure is CircleFigure);
         }
         [TestCase()]
-        public void LineFigureCreatorTest()
+        public void CircleFigureCreatorTest_WithFalseAssert()
+        {
+            CircleFigure circleFigure = new CircleFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(circleFigure is PolygonFigure);
+        }
+        [TestCase()]
+        public void LineFigureCreatorTest_WithTrueAssert()
         {
             LineFigure lineFigure = new LineFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(lineFigure is LineFigure);
         }
         [TestCase()]
-        public void PencilFigureCreatorTest()
+        public void LineFigureCreatorTest_WithFalseAssert()
+        {
+            LineFigure lineFigure = new LineFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(lineFigure is PolygonFigure);
+        }
+        [TestCase()]
+        public void PencilFigureCreatorTest_WithTrueAssert()
         {
             PencilFigure pencilFigure = new PencilFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(pencilFigure is PencilFigure);
         }
         [TestCase()]
-        public void PolygonFigureCreatorTest()
+        public void PencilFigureCreatorTest_WithFalseAssert()
+        {
+            PencilFigure pencilFigure = new PencilFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(pencilFigure is PolygonFigure);
+        }
+        [TestCase()]
+        public void PolygonFigureCreatorTest_WithTrueAssert()
         {
             PolygonFigure polygonFigure = new PolygonFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(polygonFigure is PolygonFigure);
         }
         [TestCase()]
-        public void SquareFigureCreatorTest()
+        public void PolygonFigureCreatorTest1_WithFalseAssert()
+        {
+            PolygonFigure polygonFigure = new PolygonFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(polygonFigure is LineFigure);
+        }
+        [TestCase()]
+        public void SquareFigureCreatorTest_WithTrueAssert()
         {
             SquareFigure squareFigure = new SquareFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(squareFigure is SquareFigure);
         }
         [TestCase()]
-        public void TriangleFigureCreatorTest()
+        public void SquareFigureCreatorTest_WithFalseAssert()
+        {
+            SquareFigure squareFigure = new SquareFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(squareFigure is LineFigure);
+        }
+        [TestCase()]
+        public void TriangleFigureCreatorTest_WithTrueAssert()
         {
             TriangleFigure triangleFigure = new TriangleFigure(new List<System.Drawing.Point>());
             Assert.IsTrue(triangleFigure is TriangleFigure);
+        }
+        [TestCase()]
+        public void TriangleFigureCreatorTest_WithFalseAssert()
+        {
+            TriangleFigure triangleFigure = new TriangleFigure(new List<System.Drawing.Point>());
+            Assert.IsFalse(triangleFigure is LineFigure);
         }
     }
 }
